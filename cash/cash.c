584419ac3810e260027279e34ab3caffce379c39
Chigozie Nkwocha <bueze25@gmail.com>
2020-02-07 06:01:14 +0100

    Add files via upload

---
+++ cash/cash.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include  <stdlib.h>
+
+int main(void)
+{
+    float money;
+    int quarter = 25;
+    int dime = 10;
+    int fives = 5;
+    int ones = 1;
+    int result = 0;
+
+    printf("Change: ");
+    scanf("%f", &money);
+
+    //converting to kobo and removing thedecimal part
+    int coins = 100 * money;
+    while (coins >=  0)
+    {
+        if (coins >=  quarter)
+        {
+            coins -= quarter;
+            result +=1;
+        }
+        else if (coins >= dime)
+        {
+            coins -= dime;
+            result += 1;
+        }
+        else if (coins >= fives)
+        {
+            coins -= fives;
+            result += 1;
+        }
+        else if (coins >= ones)
+        {
+            coins -= ones;
+            result += 1;
+        }
+        else
+        {
+            break;
+        }
+    }
+    printf("%d\n", result);
+    return 1;
+}
